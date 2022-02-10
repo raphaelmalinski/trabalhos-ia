5f70ca6b7d88f9615ab8c0dea44b6502c22f7308
Arthurk12 <arthur_bockmann@hotmail.com>
2022-02-10 20:41:54 -0300

    Add sucessor function
    Yet not tested

--- trabalho1/solucao.py
+++ trabalho1/solucao.py
@@ -23,7 +23,55 @@ def sucessor(estado):
     :return:
     """
     # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    PUZZLE_SIZE=3
+    ACTIONS = ["acima", "esquerda", "abaixo", "direita"]
+    EMPTY_MARK = '_'
+    
+    def isRightEdge(emptyPosition):
+      return emptyPosition%PUZZLE_SIZE == PUZZLE_SIZE - 2
+
+    def isLeftEdge(emptyPosition):
+      return emptyPosition%PUZZLE_SIZE == 0
+
+    def isBottomRow(emptyPosition):
+      return emptyPosition > (PUZZLE_SIZE*PUZZLE_SIZE) - (PUZZLE_SIZE)
+
+    def isTopRow(emptyPosition):
+      return emptyPosition < PUZZLE_SIZE - 1
+    
+    emptyPosition = estado.find(EMPTY_MARK)
+    actionsList = []
+
+    if(not isTopRow(emptyPosition=emptyPosition)):
+      #TEM AÇÃO ACIMA
+      newState = estado
+      newState[emptyPosition] = newState[emptyPosition - 3]
+      newState[emptyPosition - 3] = newState[emptyPosition]
+      actionsList.append((ACTIONS[1], newState))
+
+    if(not isBottomRow(emptyPosition=emptyPosition)):
+      #TEM AÇÃO ABAIXO
+      newState = estado
+      newState[emptyPosition] = newState[emptyPosition + 3]
+      newState[emptyPosition + 3] = newState[emptyPosition]
+      actionsList.append((ACTIONS[3], newState))
+
+    if(not isRightEdge(emptyPosition=emptyPosition)):
+      #TEM AÇÃO DIREITA
+      newState = estado
+      newState[emptyPosition] = newState[emptyPosition + 1]
+      newState[emptyPosition + 1] = newState[emptyPosition]
+      actionsList.append((ACTIONS[4]), newState)
+
+
+    if(not isLeftEdge(emptyPosition=emptyPosition)):
+      #TEM AÇÃO ESQUERDA
+      newState = estado
+      newState[emptyPosition] = newState[emptyPosition - 1]
+      newState[emptyPosition -1] = newState[emptyPosition]
+      actionsList.append((ACTIONS[2]), newState)
+  
+    return actionsList
 
 
 def expande(nodo):
