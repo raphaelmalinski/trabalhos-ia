f72edcd79354178cc1ebc68115fa26cdcff352c3
Raphael Malinski <raphaelmalinski@gmail.com>
2022-03-22 21:16:30 -0300

    Creates fit function

--- trabalho3/alegrete.py
+++ trabalho3/alegrete.py
@@ -1,14 +1,6 @@
 import numpy as np
 
 
-def retorna_colunas_matriz(m):
-    qtd_dados = np.shape(m)[0]
-
-    dados_x = list(map(lambda x: x[0], m[0:qtd_dados]))
-    dados_y = list(map(lambda y: y[1], m[0:qtd_dados]))
-
-    return dados_x, dados_y
-
 def calcula_hipotese(theta_0, theta_1, dados_x):
     return list(map(lambda x: theta_0 + theta_1 * x, dados_x))
 
@@ -21,7 +13,7 @@ def compute_mse(theta_0, theta_1, data):
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :return: float - o erro quadratico medio
     """
-    dados_x, dados_y = retorna_colunas_matriz(data)
+    dados_x, dados_y = data[:,0], data[:,1]
     dados_x_hipotese = calcula_hipotese(theta_0, theta_1, dados_x)
 
     return np.square(np.subtract(dados_x_hipotese, dados_y)).mean()
@@ -36,7 +28,7 @@ def step_gradient(theta_0, theta_1, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
     """
-    dados_x, dados_y = retorna_colunas_matriz(data)
+    dados_x, dados_y = data[:,0], data[:,1]
     dados_x_hipotese = calcula_hipotese(theta_0, theta_1, dados_x)
 
     derivada_theta_0 = 2 * np.subtract(dados_x_hipotese, dados_y).mean()
@@ -63,4 +55,11 @@ def fit(data, theta_0, theta_1, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os theta_0 e outra com os theta_1 obtidos ao longo da execução
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    lista_thetas_0, lista_thetas_1 = [], []
+    novo_theta_0, novo_theta_1 = theta_0, theta_1
+    for _ in range(num_iterations):
+        novo_theta_0, novo_theta_1 = step_gradient(novo_theta_0, novo_theta_1, data, alpha)
+        lista_thetas_0.append(novo_theta_0)
+        lista_thetas_1.append(novo_theta_1)
+
+    return lista_thetas_0, lista_thetas_1
